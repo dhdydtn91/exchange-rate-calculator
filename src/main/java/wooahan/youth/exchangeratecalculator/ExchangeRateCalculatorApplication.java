4b21fec4622a862fd51c1240e38070df46ae1b7b
Oh yong Soo <dhdydtn2019@gmail.com>
2022-06-08 21:51:02 +0900

    Initial commit

---
+++ src/main/java/wooahan/youth/exchangeratecalculator/ExchangeRateCalculatorApplication.java
@@ -0,0 +1,13 @@
+package wooahan.youth.exchangeratecalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeRateCalculatorApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ExchangeRateCalculatorApplication.class, args);
+  }
+
+}
---
+++ src/test/java/wooahan/youth/exchangeratecalculator/ExchangeRateCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package wooahan.youth.exchangeratecalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeRateCalculatorApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
